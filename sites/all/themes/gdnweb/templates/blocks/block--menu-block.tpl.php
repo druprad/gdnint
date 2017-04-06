81f03596fb11080758a3907237f3e41baefa0b57
rk <rkwork3030@gmail.com>
2017-04-06 16:28:46 +0530

    Updated footer menu's wrapper html code.

--- sites/all/themes/gdnweb/templates/blocks/block--menu-block.tpl.php
+++ sites/all/themes/gdnweb/templates/blocks/block--menu-block.tpl.php
@@ -44,14 +44,13 @@
  * @ingroup themeable
  */
 ?>
-<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
 
   <?php print render($title_prefix); ?>
 <?php if ($block->subject): ?>
-  <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
+  <h4<?php print $title_attributes; ?>><?php print $block->subject ?></h4>
 <?php endif;?>
   <?php print render($title_suffix); ?>
 
 
   <?php print $content ?>
-</div>
