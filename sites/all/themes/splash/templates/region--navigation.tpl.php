e4b23ff0476bb658114a05a72382c8a32bd1ba04
Ankur <ankurshah56@gmail.com>
2017-04-14 16:49:19 +0530

    change for Navigation and Tag line

--- sites/all/themes/splash/templates/region--navigation.tpl.php
+++ sites/all/themes/splash/templates/region--navigation.tpl.php
@@ -1,21 +1,16 @@
-<?php
-if ($content):
-  $search_block = false;
-  $blocks = block_get_blocks_by_region('search');
-  if ($blocks) {
-    foreach ($blocks as $key => $block) {
-      if ($key == 'search_form') {
-        $search_block = true;
-      }
-    }
-  }
-  ?>
-  <?php if ($search_block): ?>
-    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-2 pull-right right search-toggle-bar">
-      <a href="#" class="search-toggle"><span class="fa fa-search"></span></a>
+<div class="dexp-grid-item col-lg-8 col-md-8 col-sm-8 col-xs-12 headeright">
+  <div class="tagline"><p> <a class="search-toggle" href="#"><i class="fa fa-search"></i></a>
+    <?php
+    $site_slogan = variable_get('site_slogan');
+    if ($site_slogan): ?>
+      <span><?php print $site_slogan; ?></span><!--tagline-->
+    <?php endif; ?></p>
+  </div><!--tagline-->
+  <div class="navbar-collapse collapse">
+    <div class="region-navigation navbar-header">
+      <?php if ($content): ?>
+          <?php print $content;?>
+      <?php endif; ?>
     </div>
-    <?php endif; ?>
-  <div class="<?php print $classes; ?>">
-  <?php print $content; ?>
-  </div>
-<?php endif; ?>
+  </div><!--navbarstatic-->
+</div>
