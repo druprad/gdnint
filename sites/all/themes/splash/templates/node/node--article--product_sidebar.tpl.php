03f6a2e9f6542ef5dfdc7b3152ddb662eece9a90
Ankur <ankurshah56@gmail.com>
2017-05-22 21:49:53 +0530

    Removed Print_r from code

--- sites/all/themes/splash/templates/node/node--article--product_sidebar.tpl.php
+++ sites/all/themes/splash/templates/node/node--article--product_sidebar.tpl.php
@@ -79,12 +79,6 @@
 * @ingroup themeable
 */
 ?>
-<?php
-echo '<pre>';
-print_r($node);
-echo '</pre>';
-
-?>
 <div id="node-<?php print $node->nid; ?>" class="post <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <?php print render($title_prefix); ?>
   <?php print render($title_suffix); ?>
